596873fcd8a56372c23813e92b8849236141d8bf
Samarth Pillai <pillaisamarth@gmail.com>
2026-05-29 02:10:40 +0530

    fix redis integration and test with docker redis

--- simple-kafka-order/src/main/java/com/sp/sko/model/TokenBucket.java
+++ simple-kafka-order/src/main/java/com/sp/sko/model/TokenBucket.java
@@ -1,23 +1,36 @@
 package com.sp.sko.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.PersistenceCreator;
 import org.springframework.data.redis.core.RedisHash;
 
 import java.io.Serializable;
 
 @RedisHash("tokenBucket")
 public class TokenBucket implements Serializable {
-    private final String id;
+
+    @Id
+    private final String userId;
     private final long capacity;
     private double availableTokens;
     private long lastRefillMillis;
     private final double refillTokenPerMillis;
 
-    public TokenBucket(String id, long capacity, long refillTokens, long refillPeriodMillis){
-        this.id = id;
+    public TokenBucket(String userId, long capacity, long refillTokens, long refillPeriodMillis){
+        this.userId = userId;
         this.capacity = capacity;
         availableTokens = capacity;
         lastRefillMillis = System.currentTimeMillis();
         refillTokenPerMillis = (double) refillTokens / (double) refillPeriodMillis;
+    }
+
+    @PersistenceCreator
+    public TokenBucket(String userId, long capacity, double availableTokens, double refillTokenPerMillis){
+        this.userId = userId;
+        this.capacity = capacity;
+        this.availableTokens = availableTokens;
+        this.lastRefillMillis = System.currentTimeMillis();
+        this.refillTokenPerMillis = refillTokenPerMillis;
     }
 
     synchronized public boolean tryConsume(int numberTokens){
